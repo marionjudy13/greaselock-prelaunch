6a93c95791d72e26249768ad3ae95cf72de0989b
Marion Judy <marionejudy@gmail.com>
2024-04-16 09:05:10 -0500

    testing a change in code to update favicon

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { Hero } from "@/components/Hero";
 export default function Home() {
   return (
     <main className="flex flex-col flex-auto">
+      {/* Testing */}
       <Hero />
     </main>
   );
