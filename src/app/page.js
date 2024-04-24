455c38951ac8864f30a7ce20399f810c9275230d
Marion Judy <marionejudy@gmail.com>
2024-04-24 09:38:57 -0500

    Updated product image

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col flex-auto">
-      {/* Testing */}
       <Hero />
     </main>
   );
