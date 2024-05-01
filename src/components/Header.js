e264c6462d4346a7cfd7487ad71da7b5c3c883e4
Marion Judy <marionejudy@gmail.com>
2024-05-01 15:26:48 -0500

    added youtube icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import Image from "next/image";
+import GrYoutube from "@meronex/icons/gr/GrYoutube";
 
 export const Header = () => {
   return (
     <header className="relative shadow-md shadow-black/25 layout-padding py-4 z-10">
-      <div className="wrapper">
+      <div className="wrapper flex justify-between">
         <Image
           src="/images/greaselock-tradesmark.png"
           width={150}
           height={150}
           alt="Picture of the author"
         />
+        <a
+          href="https://www.youtube.com/@greaselock"
+          target="_blank"
+          className="flex align-middle items-center"
+        >
+          <GrYoutube size="2em" />
+        </a>
       </div>
     </header>
   );
