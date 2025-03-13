8e747757bb947a24b84bbf0c5dc3ad4d8fbaf521
Marion Judy <marionejudy@gmail.com>
2025-03-13 10:21:07 -0500

    hubspot mobile styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ export const Contact = () => {
   return (
     <section className="px-8 py-20 bg-gray">
       <div className="wrapper">
-        <div>
-          <h3 className="font-extrabold text-2xl mb-8">Contact Us</h3>
+        <div className="bg-white pt-10">
+          <h3 className="font-extrabold text-2xl ml-9">Contact Us</h3>
           <HubSpotForm />
         </div>
       </div>
--- src/components/HubspotForm.js
+++ src/components/HubspotForm.js
@@ -1,20 +1,32 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const HubSpotForm = () => {
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-
   useEffect(() => {
-    if (scriptLoaded) return; // If the script is already loaded, do nothing
+    // Check if the HubSpot script is already loaded globally
+    if (window.hbspt) {
+      // If the script is already loaded, initialize the form directly
+      window.hbspt.forms.create({
+        portalId: "22615991",
+        formId: "25300bc4-e616-44e7-b174-99bd5c21f8c8",
+        target: "#hubspot-form",
+      });
+      return; // Exit early to avoid appending the script again
+    }
 
+    // Check if the script is already in the DOM
+    if (document.querySelector("script[src='//js.hsforms.net/forms/v2.js']")) {
+      return; // If the script is already in the DOM, do nothing
+    }
+
+    // Append the script to the DOM
     const script = document.createElement("script");
     script.src = "//js.hsforms.net/forms/v2.js";
     script.async = true;
     document.body.appendChild(script);
 
     script.addEventListener("load", () => {
-      setScriptLoaded(true); // Mark the script as loaded
       if (window.hbspt) {
         window.hbspt.forms.create({
           portalId: "22615991",
@@ -28,7 +40,7 @@ const HubSpotForm = () => {
     return () => {
       document.body.removeChild(script);
     };
-  }, [scriptLoaded]);
+  }, []);
 
   return (
     <div>
