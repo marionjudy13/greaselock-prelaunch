48f7865dc4791c3a60357cab18f52ebb0a67a8a1
Marion Judy <marionejudy@gmail.com>
2025-03-14 10:43:44 -0500

    contact section styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ export const Contact = () => {
   return (
     <section className="px-8 py-24 bg-gray">
       <div className="wrapper">
-        <div className="bg-white pt-10 lg:py-20 lg:px-14">
+        <div className="bg-white py-10 lg:pt-20 lg:pb-16 lg:px-14">
           <h3 className="font-extrabold text-2xl ml-9">Contact Us</h3>
           <div className="-mt-4">
             <HubSpotForm />
