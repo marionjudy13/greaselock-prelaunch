4870928198246538c14a31f04c3c4a580bf728c4
Marion Judy <marionejudy@gmail.com>
2025-03-17 13:47:39 -0500

    fixed pdf button link function

--- src/components/Pdfs.js
+++ src/components/Pdfs.js
@@ -10,68 +10,68 @@ export const Pdfs = () => {
           </h3>
           <ul className="-translate-x-1 lg:flex justify-between">
             <li className="mb-5 group">
-              <div className="background" />
-              <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
-                <Link
-                  href="/pdfs/Grease Lock Care Guide.pdf"
-                  target="_blank"
-                  className="no-underline"
-                >
+              <Link
+                href="/pdfs/Grease Lock Care Guide.pdf"
+                target="_blank"
+                className="no-underline"
+              >
+                <div className="background" />
+                <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
                   Care Guide
-                </Link>
-                <img
-                  src="images/arrow-right-solid.svg"
-                  className="arrow duration-300 ease-in-out group-hover:translate-x-1"
-                />
-              </div>
+                  <img
+                    src="images/arrow-right-solid.svg"
+                    className="arrow duration-300 ease-in-out group-hover:translate-x-1"
+                  />
+                </div>
+              </Link>
             </li>
             <li className="mb-5 group">
-              <div className="background" />
-              <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
-                <Link
-                  href="pdfs/Grease Lock Overview & Spec Sheet.pdf"
-                  target="_blank"
-                  className="no-underline"
-                >
+              <Link
+                href="pdfs/Grease Lock Overview & Spec Sheet.pdf"
+                target="_blank"
+                className="no-underline"
+              >
+                <div className="background" />
+                <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
                   Overview & Spec Sheet
-                </Link>
-                <img
-                  src="images/arrow-right-solid.svg"
-                  className="arrow duration-300 ease-in-out group-hover:translate-x-1"
-                />
-              </div>
+                  <img
+                    src="images/arrow-right-solid.svg"
+                    className="arrow duration-300 ease-in-out group-hover:translate-x-1"
+                  />
+                </div>
+              </Link>
             </li>
             <li className="mb-5 group">
-              <div className="background" />
-              <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
-                <Link
-                  href="pdfs/Grease Lock Product Comparison Sheet.pdf"
-                  target="_blank"
-                  className="no-underline"
-                >
+              <Link
+                href="pdfs/Grease Lock Product Comparison Sheet.pdf"
+                target="_blank"
+                className="no-underline"
+              >
+                <div className="background" />
+                <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
                   Product Comparison Sheet
-                </Link>
-                <img
-                  src="images/arrow-right-solid.svg"
-                  className="arrow duration-300 ease-in-out group-hover:translate-x-1"
-                />
-              </div>
+                  <img
+                    src="images/arrow-right-solid.svg"
+                    className="arrow duration-300 ease-in-out group-hover:translate-x-1"
+                  />
+                </div>
+              </Link>
             </li>
             <li className="group">
-              <div className="background" />
-              <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
-                <Link
-                  href="pdfs/Grease Lock Sell Sheet.pdf"
-                  target="_blank"
-                  className="no-underline"
-                >
+              <Link
+                href="pdfs/Grease Lock Sell Sheet.pdf"
+                target="_blank"
+                className="no-underline"
+              >
+                <div className="background" />
+                <div className="pdf bg-white text-blue text-lg font-bold text-center w-100 px-10 py-6">
                   Sell Sheet
-                </Link>
-                <img
-                  src="images/arrow-right-solid.svg"
-                  className="arrow duration-300 ease-in-out group-hover:translate-x-1"
-                />
-              </div>
+                  <img
+                    src="images/arrow-right-solid.svg"
+                    className="arrow duration-300 ease-in-out group-hover:translate-x-1"
+                  />
+                </div>
+              </Link>
             </li>
           </ul>
         </div>
